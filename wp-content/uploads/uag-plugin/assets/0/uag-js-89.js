aea19c40e3496b50cd64758a27b4dd7b063f46fa
excelspreadsheets <louisguilhem.p@gmail.com>
2024-05-24 20:54:34 +0200

    trying fixing the links again ...

--- wp-content/uploads/uag-plugin/assets/0/uag-js-89.js
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-89.js
@@ -13,19 +13,4 @@ window.addEventListener( 'load', function() {
 window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-2bc58bd8' );
 });
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-dc17c5d4' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-9dab7a4d' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-dd26be11' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-731f3752' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-2bc58bd8' );
-});
  });
