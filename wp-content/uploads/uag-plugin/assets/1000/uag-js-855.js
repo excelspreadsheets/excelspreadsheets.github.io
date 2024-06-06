159042f8ab7ee92fadd2a55064c03f8073452d9a
excelspreadsheets <louisguilhem.p@gmail.com>
2024-06-06 16:55:15 +0200

    Changes on Clairvaux's structure

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-855.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-dc17c5d4' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9dab7a4d' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-dd26be11' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-731f3752' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-2bc58bd8' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-2aa13ff1 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-2aa13ff1 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-2aa13ff1 .swiper-button-next","prevEl":".uagb-block-2aa13ff1 .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-bafcdb7c .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-bafcdb7c .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-bafcdb7c .swiper-button-next","prevEl":".uagb-block-bafcdb7c .swiper-button-prev"}}	);
+});
+
+ });
