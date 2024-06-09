f67989c5b4307949a4b4d395fbf05af346eca621
excelspreadsheets <louisguilhem.p@gmail.com>
2024-06-09 16:12:29 +0200

    Fixing up and styling home page

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-971.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-971.js
@@ -1,19 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-dc17c5d4' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-9dab7a4d' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-dd26be11' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-731f3752' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-2bc58bd8' );
-});
-jQuery(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function(){ jQuery(document).ready(function () {
 				let scope = jQuery(".wp-block-uagb-image-gallery.uagb-block-34a6dac4");
 				if ( scope.length ) { 
 					scope.css("visibility", "visible");
