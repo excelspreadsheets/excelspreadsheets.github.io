a70f26df79a95e940872435e0b2ac113859c860c
excelspreadsheets <louisguilhem.p@gmail.com>
2024-07-04 23:18:21 +0200

    Added a couple of pages and fixed Clairvaux page. Made posts more content friendly

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-518.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-518.js
@@ -1,16 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-dc17c5d4' );
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-0083031d .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":false,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"fade","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-0083031d .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-0083031d .swiper-button-next","prevEl":".uagb-block-0083031d .swiper-button-prev"}}	);
 });
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-9dab7a4d' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-dd26be11' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-731f3752' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-2bc58bd8' );
-});
+
  });
