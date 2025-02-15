e77b46a416e473d522d6c19b313456ae02f28302
excelspreadsheets <louisguilhem.p@gmail.com>
2025-02-15 16:51:59 +0100

    RE
    Changement de clairvaux
    supression des Sidemenu Pages

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-518.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-518.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	var swiper = new Swiper( ".uagb-block-0083031d .uagb-swiper",
-		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":false,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"fade","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-0083031d .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-0083031d .swiper-button-next","prevEl":".uagb-block-0083031d .swiper-button-prev"}}	);
+	var swiper = new Swiper( ".uagb-block-bef7d64d .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-bef7d64d .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-bef7d64d .swiper-button-next","prevEl":".uagb-block-bef7d64d .swiper-button-prev"}}	);
 });
 
  });
