d770dbe9a437151cc9e82f1f119af82fc1ba6151
excelspreadsheets <louisguilhem.p@gmail.com>
2025-05-03 12:58:06 +0200

    Font change, PFe change

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-855.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-855.js
@@ -8,9 +8,4 @@ window.addEventListener("DOMContentLoaded", function(){
 		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-bafcdb7c .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-bafcdb7c .swiper-button-next","prevEl":".uagb-block-bafcdb7c .swiper-button-prev"}}	);
 });
 
-window.addEventListener("DOMContentLoaded", function(){
-	var swiper = new Swiper( ".uagb-block-18273166 .uagb-swiper",
-		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-18273166 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-18273166 .swiper-button-next","prevEl":".uagb-block-18273166 .swiper-button-prev"}}	);
-});
-
  });
